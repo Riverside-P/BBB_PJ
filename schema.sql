91b2c026863c50ed74f496c33d937a67c9ccdb7c
23fi090 <23fi090@ms.dendai.ac.jp>
2026-02-03 14:27:52 +0900

    リンクテーブルの作成

--- schema.sql
+++ schema.sql
@@ -17,3 +17,33 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- 今後、ステップ5や7で必要になる transfers や requests テーブルも
 -- 決まり次第、このファイルに追記して共有しましょう。
+
+-- リンク情報を管理するテーブル
+-- 外部キー制約を有効にする（セッションごとに必要）
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE links (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
+    -- 0か1のみを許容する制約
+    -- 0ならリンクが有効、1なら無効
+    status INTEGER CHECK (status IN (0, 1)),
+    
+    -- usersテーブルのnameカラムを参照する外部キー
+    requester TEXT,
+    
+    -- NULLを許容しない
+    payer TEXT NOT NULL,
+    
+    -- 1以上かつNULLを許容しない
+    amount INTEGER NOT NULL CHECK (amount >= 1),
+    
+    -- 文字列型
+    comment TEXT,
+    
+    -- デフォルトで現在時刻が入るタイムスタンプ
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    -- 外部キーの設定
+    FOREIGN KEY (requester) REFERENCES users(account_number)
+);
