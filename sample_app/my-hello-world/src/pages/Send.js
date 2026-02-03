e87bee2ed4745d4680a855442396bdbeb459e0a2
Riverside-P <a.kawaide.math@gmail.com>
2026-02-03 14:46:05 +0900

    fix : ディレクトリ構成を変更

--- sample_app/my-hello-world/src/pages/Send.js
+++ sample_app/my-hello-world/src/pages/Send.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Send.css'; // ← パスはあなたの環境に合わせて (../Send.css かも)
+import '../styles/Send.css';
 
 function Send() {
   const navigate = useNavigate();
